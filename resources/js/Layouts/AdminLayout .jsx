abfc469f8b552d07c5c8d0067d8d762ae9d3d004
Edem Kwaku <edemkwakuavorley@gmail.com>
2024-08-22 12:02:32 +0000

    deleted some jsx files

--- resources/js/Layouts/AdminLayout .jsx
+++ resources/js/Layouts/AdminLayout .jsx
@@ -53,55 +53,49 @@ export default function Authenticated({ user, header, children }) {
                                     >
                                         Collaboration
                                     </NavLink>
-                                    {console.log(hasRole("admin"))}
-                                    {hasRole("admin") && (
-                                        <div className="flex items-center justify-center mt-4">
-                                            <Dropdown className="flex items-center justify-center  ml-6">
-                                                <Dropdown.Trigger>
-                                                    <span className="inline-flex  rounded-md">
-                                                        <button
-                                                            type="button"
-                                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+
+                                    <div className="flex items-center justify-center mt-4">
+                                        <Dropdown className="flex items-center justify-center  ml-6">
+                                            <Dropdown.Trigger>
+                                                <span className="inline-flex  rounded-md">
+                                                    <button
+                                                        type="button"
+                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                    >
+                                                        <span className="px-2">
+                                                            Management
+                                                        </span>
+
+                                                        <svg
+                                                            className="ms-2 -me-0.5 h-4 w-4"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor"
                                                         >
-                                                            <span className="px-2">
-                                                                Management
-                                                            </span>
+                                                            <path
+                                                                fillRule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clipRule="evenodd"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </Dropdown.Trigger>
 
-                                                            <svg
-                                                                className="ms-2 -me-0.5 h-4 w-4"
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                viewBox="0 0 20 20"
-                                                                fill="currentColor"
-                                                            >
-                                                                <path
-                                                                    fillRule="evenodd"
-                                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                                    clipRule="evenodd"
-                                                                />
-                                                            </svg>
-                                                        </button>
-                                                    </span>
-                                                </Dropdown.Trigger>
-
-                                                <Dropdown.Content>
-                                                    <Dropdown.Link
-                                                        href={route(
-                                                            "admin.users"
-                                                        )}
-                                                    >
-                                                        Users
-                                                    </Dropdown.Link>
-                                                    <Dropdown.Link
-                                                        href={route(
-                                                            "admin.roles"
-                                                        )}
-                                                    >
-                                                        Roles
-                                                    </Dropdown.Link>
-                                                </Dropdown.Content>
-                                            </Dropdown>
-                                        </div>
-                                    )}
+                                            <Dropdown.Content>
+                                                <Dropdown.Link
+                                                    href={route("admin.users")}
+                                                >
+                                                    Users
+                                                </Dropdown.Link>
+                                                <Dropdown.Link
+                                                    href={route("admin.roles")}
+                                                >
+                                                    Roles
+                                                </Dropdown.Link>
+                                            </Dropdown.Content>
+                                        </Dropdown>
+                                    </div>
                                 </div>
 
                                 <div className="-me-2 flex items-center sm:hidden">
